ea017e54ef1369780a8922bea06641d7d401aebd
KYT88 <158803821+KYT88@users.noreply.github.com>
2024-05-18 13:42:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 window.addEventListener("load" , () =>{
     const sounds = document.querySelectorAll(".sound");
     const board = document.querySelectorAll (".board div");
+    const board2 = document.querySelectorAll (".board2 div");
 
 
     board.forEach ((aa, index) => {
         aa.addEventListener("click",function() {
             sounds[index].currentTime = 0;
             sounds[index].play();
+        });
+    });
+
+    board2.forEach ((bb, index1) => {
+        bb.addEventListener("click",function() {
+            sounds[index1].currentTime = 0;
+            sounds[index1].play();
         });
     });
 });
